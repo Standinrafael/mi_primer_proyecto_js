79e227eb2d9fef7fe02e347f6d7265bd73adab75
jlsan <jlsan@DESKTOP-KA149H8>
2021-11-29 20:04:55 -0500

    El primer proyecto que hicimos en clase con GitHub

---
+++ src/main/java/mi/primer/proyecto/js/Animal.java
@@ -0,0 +1,11 @@
+package mi.primer.proyecto.js;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto con GitHub");
+
+	}
+
+}
