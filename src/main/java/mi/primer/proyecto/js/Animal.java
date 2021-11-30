1fb5a773939c2ec95c3d33f2daa298d7a837454b
jlsan <jlsan@DESKTOP-KA149H8>
2021-11-29 20:09:10 -0500

    Segundo cambio

--- src/main/java/mi/primer/proyecto/js/Animal.java
+++ src/main/java/mi/primer/proyecto/js/Animal.java
@@ -5,6 +5,9 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi primer proyecto con GitHub");
+		System.out.println("Mi segundo cambio");
+		
+		
 
 	}
 
