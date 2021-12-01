50dedc53a98a50c02b6484fd7b252ed4f4c32ea8
jlsan <jlsan@DESKTOP-KA149H8>
2021-11-30 20:48:29 -0500

    Ejecucion de Programa con Maven

---
+++ src/main/java/mi/primer/proyecto/js/PruebaMaven.java
@@ -0,0 +1,14 @@
+package mi.primer.proyecto.js;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PruebaMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto MAVEN");
+		List <Object> nombre=new ArrayList<>();
+	}
+
+}
