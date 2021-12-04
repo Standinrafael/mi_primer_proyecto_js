817b7fff02bd11412a8595de69c17cf5cda84037
jlsan <jlsan@DESKTOP-KA149H8>
2021-12-03 19:34:35 -0500

    Dependencias entre clases( variables de instacnia)

---
+++ src/main/java/com/ec/edu/dependencias/MainUCE.java
@@ -0,0 +1,36 @@
+package com.ec.edu.dependencias;
+
+import java.util.Scanner;
+
+public class MainUCE {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Ingrese Nombre:");
+		String nombre=scanner.nextLine();
+		
+		System.out.println("Ingrese Apellido:");
+		String apellido=scanner.nextLine();
+		
+		System.out.println("Ingrese Calle:");
+		String calle=scanner.nextLine();
+		
+		System.out.println("Ingrese Numeracion:");
+		String numeracion=scanner.nextLine();
+		
+		// TODO Auto-generated method stub
+
+		Matricula matricula= new Matricula();
+		matricula.setAnio(2015);
+		matricula.setSemestre("Sexto");
+		
+		String mensaje=matricula.matricular(nombre, apellido, calle, numeracion);
+		
+		System.out.println(mensaje);
+		
+		
+	}
+
+}
---
+++ src/main/java/com/ec/edu/dependencias/Matricula.java
@@ -0,0 +1,52 @@
+package com.ec.edu.dependencias;
+
+public class Matricula {
+	
+	private String semestre;
+	private int anio;
+	
+	private Estudiante estudiante;
+	
+	public String matricular(String nombre, String apellido, String calle, String numero) {
+		this.estudiante=new Estudiante();
+		
+		
+		this.estudiante.setNombre(nombre);
+		this.estudiante.setApellido(apellido);
+		
+		
+		Direccion direccion= new Direccion();
+		direccion.setCallePrincipal(calle);
+		direccion.setNumeracion(numero);
+		this.estudiante.setDireccion(direccion);
+		//logia para guardar en la base de datos los datos de la matricula
+		//y el estudiante
+		
+		//se imprimer el espacio de memoria
+		System.out.println(this.estudiante);
+		return "Estudainte Guardado con exito";
+	}
+	
+	//Metodos SET y GET
+	public String getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+	
+	 
+
+}
