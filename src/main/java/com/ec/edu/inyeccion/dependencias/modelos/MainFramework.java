09c85ecce532c2675e02035c735103a10ee64e1b
jlsan <jlsan@DESKTOP-KA149H8>
2021-12-12 17:09:22 -0500

    IoC y DI con Modelo de Interfaz y Clase Anonima

--- src/main/java/com/ec/edu/inyeccion/dependencias/modelos/MainFramework.java
+++ src/main/java/com/ec/edu/inyeccion/dependencias/modelos/MainFramework.java
@@ -19,6 +19,23 @@ public class MainFramework {
 		}else if(tipo==3) {
 			cuenta= new CuentaFuturo();
 		}
+		else {
+			cuenta= new ICuenta(){
+
+				@Override
+				public void calcularDescuento(String numero) {
+					// TODO Auto-generated method stub
+					System.out.println("Calculando Beneficio de cuenta ANONIMA..........");
+				}
+
+				@Override
+				public void calcularBeneficio() {
+					// TODO Auto-generated method stub
+					System.out.println("Calculado Beneficio de cuenta ANONIMA............");
+				}
+				
+			};
+		}
 		
 		GestorCuentas gestor= new GestorCuentas(cuenta);
 		gestor.registrarCuentaHabiente();
