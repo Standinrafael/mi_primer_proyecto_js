33027683fef1261f204ba32a6f39309633a4dea8
Dennis <Dennis@DESKTOP-D84QG4M>
2021-12-17 19:21:26 -0500

    segunda prueba

--- src/main/java/Prueba/main.java
+++ src/main/java/Prueba/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 			System.out.println("main prueba");
-			System.out.println(" primera prueba    ");
+			System.out.println(" segunda prueba    ");
 	}
 
 }
