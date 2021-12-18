9b2a079a145b7f0543a09fd0a3b6f6d95649a0af
Dennis <Dennis@DESKTOP-D84QG4M>
2021-12-17 19:15:16 -0500

    prueba

--- src/main/java/Prueba/main.java
+++ src/main/java/Prueba/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 			System.out.println("main prueba");
+			System.out.println("as");
 	}
 
 }
