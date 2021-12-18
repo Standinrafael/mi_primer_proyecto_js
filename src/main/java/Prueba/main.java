f9423d18a4c2b98dbc45d456f9c5ac2feaefc734
Dennis <Dennis@DESKTOP-D84QG4M>
2021-12-17 19:13:19 -0500

    prueba

---
+++ src/main/java/Prueba/main.java
@@ -0,0 +1,10 @@
+package Prueba;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+			System.out.println("main prueba");
+	}
+
+}
